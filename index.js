68481e060f9c9d1279419761a5fa69558850eece
rai8 <hytonne@outlook.com>
2019-10-24 19:22:29 +0300

    event listener

---
+++ index.js
@@ -0,0 +1,8 @@
+function demo() {
+  /* var text =  */ document.getElementById("demo").innerHTML =
+    "This is my html with javascript";
+}
+function myHeading() {
+  var head = "This is another heading";
+  document.getElementById("test").innerHTML = head;
+}
