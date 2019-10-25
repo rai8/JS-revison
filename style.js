c0ebd3fd0666765a5d8d6f98c2dc4618dab45da4
rai8 <hytonne@outlook.com>
2019-10-25 14:19:12 +0300

    styling, hiding and showing

---
+++ style.js
@@ -0,0 +1,10 @@
+function myStyle() {
+  document.getElementById("demo").style.fontSize = "40px";
+  document.getElementById("demo").style.color = "blue";
+}
+function hideDisplay() {
+  document.getElementById("display").style.display = "none";
+}
+function showDisplay() {
+  document.getElementById("show").style.display = "block";
+}
